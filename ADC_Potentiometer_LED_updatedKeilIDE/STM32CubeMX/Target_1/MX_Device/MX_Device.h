9f0e08007f19c19ba81856d99016fb6e84b1c826
khoffy <adolf_hermann86@yahoo.fr>
2024-12-02 08:49:43 -0500

    New ADC_Potentiometer_LED version due to the Keil IDE update. This comes with a new file organization.

---
+++ ADC_Potentiometer_LED_updatedKeilIDE/STM32CubeMX/Target_1/MX_Device/MX_Device.h
@@ -0,0 +1,16 @@
+/******************************************************************************
+ * File Name   : MX_Device.h
+ * Date        : 02/12/2024 08:14:54
+ * Description : STM32Cube MX parameter definitions
+ * Note        : This file is generated with a generator out of the
+ *               STM32CubeMX project and its generated files (DO NOT EDIT!)
+ ******************************************************************************/
+
+#ifndef MX_DEVICE_H__
+#define MX_DEVICE_H__
+
+/* MX_Device.h version */
+#define MX_DEVICE_VERSION                       0x01000000
+
+
+#endif  /* MX_DEVICE_H__ */
